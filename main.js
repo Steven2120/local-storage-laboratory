fe13b116ab60792b8171a3a7a70ede456b9cee30
Steven Moyano <stevenmoyano1@gmail.com>
2022-04-16 16:33:33 -0500

    completed required tasks

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ let button1 = document.querySelector(".btn__1");
 let display1 = document.querySelector(".display__text1");
 let refresherMessage = document.querySelector(".refresher__message");
 let refresherCounter = document.querySelector(".counter__refresher");
+let input3 = document.querySelector(".input__3");
+let button3 = document.querySelector(".btn__3");
+let ordered = document.querySelector(".chall__3");
 
 const saveText1 = () => {
   localStorage.setItem("text-1", input1.value);
@@ -19,3 +22,30 @@ const counterRefresherFunc = () => {
 
 refresherCounter.innerText =
   localStorage.getItem("counter") <= 0 ? 0 : localStorage.getItem("counter");
+
+//CHALLENGE 3
+let textArr = JSON.parse(localStorage.getItem("textArr")) || [];
+
+const saveText3 = () => {
+  textArr = JSON.parse(localStorage.getItem("textArr"));
+  textArr.push(input3.value);
+  localStorage.setItem("textArr", JSON.stringify(textArr));
+
+  var li = document.createElement("li");
+
+  li.innerText = input3.value;
+
+  ordered.appendChild(li);
+};
+
+button3.addEventListener("click", saveText3);
+
+if (textArr) {
+  for (let i = 0; i < JSON.parse(localStorage.getItem("textArr")).length; i++) {
+    var li = document.createElement("li");
+
+    li.innerHTML = JSON.parse(localStorage.getItem("textArr"))[i];
+
+    ordered.appendChild(li);
+  }
+}
